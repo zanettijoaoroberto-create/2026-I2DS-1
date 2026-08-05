c42132979c831c225398d10b8f88f5ede439a077
zanettijoaoroberto-create <zanettijoaoroberto@gmail.com>
2026-08-05 09:25:13 -0300

    Aula 03

--- Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
+++ Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+056ef6b070bbb993772490ce3dee5fb9cc80c681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3eb3eea69302f91358a92e0d7bb369b865dee83")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+056ef6b070bbb993772490ce3dee5fb9cc80c681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3eb3eea69302f91358a92e0d7bb369b865dee83")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
